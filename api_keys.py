193625a1721e1dce2b44a5d5fc1b61e8093b8935
Laura M <lmm9@users.noreply.github.com>
2020-08-30 01:01:45 -0400

    Update api_keys.py
    removed data

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "faa5c3f631225f07954b507a3255a499"
+weather_api_key = "weather_api_key"
 
 # Google API Key
-g_key = "AIzaSyAKF7JEa0H1eihrQRAAtWluKydxN1cybiE"
+g_key = "google_api_key"
